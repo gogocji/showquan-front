3a2a5c6b8a28638dd59010e80d9e7016d3f19ddd
gogocj <1329110070@qq.com>
2022-05-02 22:01:07 +0800

    feat: 文章列表不同入场特效

--- components/ListItem/index.tsx
+++ components/ListItem/index.tsx
@@ -5,20 +5,28 @@ import { Avatar } from 'antd';
 import { EyeOutlined } from '@ant-design/icons';
 import { markdownToTxt } from 'markdown-to-txt';
 import styles from './index.module.scss';
+import { useEffect, useState } from 'react'
+import { observer } from "mobx-react-lite"
 
 interface IProps {
   article: IArticle;
 }
-
+var globalWidth : any = 0;
 const ListItem = (props: IProps) => {
   const { article } = props;
   const { user } = article;
-
+  const [screenWidth, setScreenWidth] = useState(0)
+  // 只有在用户端才执行,服务端不执行
+  useEffect(() => {
+    // 服务端无法访问document和window变量
+    globalWidth = document.documentElement.clientWidth || document.body.clientWidth;
+    setScreenWidth(globalWidth)
+  }, globalWidth)
   return (
     // eslint-disable-next-line @next/next/link-passhref
-    <Link href={`/article/${article.id}`}>
+    <Link href={`/article/${article.id}`} >
       <div className={styles.container}>
-        <div className={styles.article}>
+        <div className={(styles.article, screenWidth >=770?styles.cssnice1:styles.cssnice)}>
           <div className={styles.userInfo}>
             <span className={styles.name}>{user?.nickname}</span>
             <span className={styles.date}>
@@ -38,4 +46,4 @@ const ListItem = (props: IProps) => {
   );
 };
 
-export default ListItem;
+export default observer(ListItem);
