a3923798c20e429c206780c982f7b397ef920154
gogocj <1329110070@qq.com>
2022-05-02 17:12:18 +0800

    feat: 偷笑旋转切换

--- components/Author/index.tsx
+++ components/Author/index.tsx
@@ -7,7 +7,7 @@ import { useStore } from 'store/index';
 interface IProps {
   userInfo: IUserInfo
 }
-
+var imgDeg = 0
 const CountDown = (props: IProps) => {
   const { userInfo } = props;
   const store = useStore()
@@ -15,16 +15,28 @@ const CountDown = (props: IProps) => {
 
   const changeAvatar = () => {
     store.common.setCommonInfo({ defstyle: !defstyle })
+    let img =document.getElementById('userImg')
+    if (defstyle) {
+      imgDeg -= 360
+      img.style.transform = 'rotate('+imgDeg+'deg)'
+      setTimeout(()=>{
+        console.log('111')
+      },300)
+    }
+    else{
+      imgDeg += 360
+      img.style.transform = 'rotate('+imgDeg+'deg)'
+    }
   }
   return (
     <div className={styles.container}>
       <div className={styles.avatar}>
-        <Avatar size={100} src={userInfo.avatar} className={styles.userlight} onMouseEnter={()=>{changeAvatar()}}/>
+        <Avatar id="userImg" size={100} src={defstyle ? '/images/avatar.jpg' :userInfo.avatar} className={styles.userlight} onMouseEnter={()=>{changeAvatar()}}/>
       </div>
-      <div className='userName' style={defstyle ?{color:'hotpink'}:{color:'rgba(0, 0, 0, 0.65)'}}>{userInfo.nickname}</div>
-      <div className='author-introduction' style={{ color: 'rgb(0,216,255)' }}>软件工程</div>
-      <div className='author-introduction myname1' style={{ color: 'rgb(0,216,255)' }}>2017-2021级学生</div>
-      <div className='author-introduction position'></div>
+      <div className={styles.userName} style={defstyle ?{color:'hotpink'}:{color:'rgba(0, 0, 0, 0.65)'}}>{userInfo.nickname}</div>
+      <div className={styles.authorIntroduction} style={{ color: 'rgb(0,216,255)' }}>软件工程</div>
+      <div className={styles.authorIntroduction} style={{ color: 'rgb(0,216,255)' }}>2019-2023级学生</div>
+      <div className={styles.authorIntroduction}></div>
     </div>
   )
 }
