086f847afe97b31c197539b2c6038328b6a33068
gogocj <1329110070@qq.com>
2022-05-02 11:23:11 +0800

    feat: header样式优化

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -3,7 +3,8 @@ import { useRouter } from 'next/router';
 import styles from './index.module.scss';
 import { navs } from './config';
 import type { NextPage } from 'next'
-import { Button, Avatar, Dropdown, Menu, message } from 'antd'
+import { Button, Avatar, Dropdown, Menu, message, Row, Col, Icon } from 'antd'
+import { HomeFilled, DownOutlined, UpOutlined } from '@ant-design/icons'
 import { useState } from 'react'
 import Login from 'components/Login/index'
 import { useStore } from 'store/index';
@@ -16,7 +17,7 @@ const Navbar: NextPage = () => {
   const { userId, avatar } = store.user.userInfo
   const { pathname, push } = useRouter();
   const [ isShowLogin, setIsShowLogin ] = useState(false);
-
+  const [ isMenuOpen, setIsMenuOpen ] = useState(false)
   const handleGotoEditorPage = () => {
     if (userId) {
       push('editor/new')
@@ -60,32 +61,53 @@ const Navbar: NextPage = () => {
   }
   
   return (
-    <div className={styles.navbar}>
-    <section className={styles.logoArea}>华农秀秀</section>
-      <section className={styles.linkArea}>
-        {navs?.map((nav) => (
-          <Link key={nav.label} href={nav.value}>
-            <a className={pathname === nav.value ? styles.active : ''}>
-              {nav.label}
-            </a>
-          </Link>
-        ))}
-      </section>
-      <section className={styles.operationArea}>
-        <Button onClick={handleGotoEditorPage}>写文章</Button>
-          {
-            userId ? (
-              <>
-                  <Dropdown overlay={renderDropDownMenu()} placement="bottomLeft">
-                    <Avatar src={avatar} size={32} />
-                  </Dropdown>
-              </>
-            ) : (
-              <Button type="primary" onClick={handleLogin}>登录</Button>
-            )
-          }
-      </section>
-      <Login isShow={isShowLogin} onClose={handleClose}/>
+    <div className={styles.header}>
+      <Row align='middle' justify="space-between" typeof='flex'>
+        <Col xs={16} sm={16} md={0} lg={0} xl={0}>
+          <span className={styles.logo}>华农秀秀</span>
+        </Col>
+        <Col xs={0} sm={0} md={5} lg={5} xl={5}>
+          <span className={styles.logo}>华农秀秀</span>
+          <span className={styles.headerTxt}>热爱生活</span>
+        </Col>
+        <Col className={styles.memuDiv} xs={4} sm={3} md={4} lg={4} xl={4}>
+          <Menu mode='horizontal'
+            style={{backgroundColor: 'rgb(40, 54, 70)'}}
+            overflowedIndicator={isMenuOpen ?<UpOutlined style={{color: 'white'}} />:<DownOutlined style={{color: 'white'}} />}
+            onOpenChange={()=>setIsMenuOpen(!isMenuOpen)}
+            >
+            <section className={styles.linkArea}>
+              {navs?.map((nav) => (
+                <Menu.Item key={nav.label}>
+                  <HomeOutlined  className={pathname === nav.value ? styles.active : styles.icon}/>
+                  <Link key={nav.label} href={nav.value}>
+                    <a className={pathname === nav.value ? styles.active : ''}>
+                      {nav.label}
+                    </a>
+                  </Link>
+                </Menu.Item>
+              ))}
+            </section>
+          </Menu>
+        </Col>
+      <Col xs={0} sm={8} md={9} lg={8} xl={5}>
+        <section className={styles.operationArea}>
+          <div className={styles.writeText} onClick={handleGotoEditorPage}>写文章</div>
+            {
+              userId ? (
+                <>
+                    <Dropdown overlay={renderDropDownMenu()} placement="bottomLeft">
+                      <Avatar src={avatar} size={32} />
+                    </Dropdown>
+                </>
+              ) : (
+                <Button type="primary" onClick={handleLogin}>登录</Button>
+              )
+            }
+        </section>
+        <Login isShow={isShowLogin} onClose={handleClose}/>
+      </Col>
+      </Row>
     </div>
   );
 };
