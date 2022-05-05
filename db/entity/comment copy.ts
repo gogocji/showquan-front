2ea33a7420d741fe8189359a2fedfab8a0ec0d76
gogocj <1329110070@qq.com>
2022-05-05 22:37:34 +0800

    feat: 点赞表

---
+++ db/entity/comment copy.ts
@@ -0,0 +1,23 @@
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
+import { Article } from './article'
+import { User } from './user'
+
+@Entity({name: 'thumbs'})
+export class Thumb extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  readonly id!: number;
+
+  @Column()
+  create_time!: Date;
+
+  @Column()
+  update_time!: Date;
+
+  @ManyToOne(() => User)
+  @JoinColumn({name: 'user_id'})
+  user!: User;
+
+  @ManyToOne(() => Article)
+  @JoinColumn({name: 'article_id'})
+  article!: Article;
+}
